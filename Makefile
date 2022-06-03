bb5fe9bdcd73cab4a11a705955e0ab936c2e2216
Lukas Krabbe <mail@l-krabbe.de>
2022-06-03 23:08:10 +0200

    add linter

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 pip/install:
 	pip-compile requirements.in
 	pip install -r requirements.txt
+
+py/lint:
+	pylint ./kn
--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-def test():
-    print('h2i')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    test()
