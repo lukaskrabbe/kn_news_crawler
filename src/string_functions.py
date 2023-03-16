69f2d1a95003bdaa3dc48e02b09394c626626be9
Lukas Krabbe <mail@l-krabbe.de>
2023-03-16 18:18:47 +0100

    feat(kn) add file writer

--- src/string_functions.py
+++ src/string_functions.py
@@ -1,6 +1,8 @@
 """
     Module used to play Around
 """
+import os
+from typing import Optional
 
 
 def double_string(text: str) -> str:
@@ -10,12 +12,30 @@ def double_string(text: str) -> str:
     Args:
         text: String to double
 
-    Returns:m
+    Returns:
         value: Value which contains the doubled String
     """
     value = text * 2
     return value
 
+def write_string_to_file(text: str, file_name: str, data_dir: Optional[str] = './../data/raw/') -> str:
+    """
+
+    Args:
+        text: String which will be written to File
+        file_name: Filename
+        data_dir: Optional data path
+
+    Returns:
+        file_path: Path to the created file
+
+    """
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    with open(data_dir + file_name, "w") as text_file:
+        text_file.write(text)
+
 
 if __name__ == "__main__":
-    print(double_string(double_string(text="Test")))
+    write_string_to_file("Hallo Welt", "test.txt")
