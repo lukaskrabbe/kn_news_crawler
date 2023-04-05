cb850bd4c4471af5aa838dd6201c4f432a059c0c
Lukas Krabbe <mail@l-krabbe.de>
2023-04-05 10:38:23 +0200

    feat(kn) add city and author handling

--- src/kn/prep.py
+++ src/kn/prep.py
@@ -33,6 +33,51 @@ def remove_multiple_spaces(value):
     return re.sub(" +", " ", value)
 
 
+def split_mulitple_authors(value):
+    value = value.replace(" und ", ", ")
+    value = value.replace("und ", ", ")
+    value = value.replace("UND ", ", ")
+    value = value.replace(" UND ", ", ")
+
+    if ", " in value:
+        value = value.split(", ")
+        tmp = {}
+        for i, val in enumerate(value):
+            tmp[f"author_{i}"] = val
+        return tmp
+    else:
+        return {"author_0": value}
+
+
+def remove_city(value):
+    match = re.search(r"^ [A-züäö\/A-züäö]*\. ", value)
+    if match:
+        city = value[match.start() : match.end()]
+        text = value[len(city) :]
+        city = city.replace(" ", "")
+        city = city.replace(".", "")
+        if "/" in city:
+            city = city.split("/")[0]
+        city = city.lower().title()
+
+        if city.lower() in [
+            "montag",
+            "dienstag",
+            "mittwoch",
+            "donnerstag",
+            "freitag",
+            "samstag",
+            "sonntag",
+        ]:
+            city = None
+
+    else:
+        text = value
+        city = None
+
+    return text, city
+
+
 def prep_kn_content(raw_content_data: Dict[str, str]) -> Dict[str, str]:
     prep_content_data = {}
 
@@ -43,11 +88,19 @@ def prep_kn_content(raw_content_data: Dict[str, str]) -> Dict[str, str]:
             value = remove_multiple_spaces(value)
             if key == "author":
                 value = remove_von(value)
+                value = split_mulitple_authors(value)
+                prep_content_data["author"] = {}
+                for key, val in value.items():
+                    prep_content_data["author"][key] = val
             if key == "page":
                 value = int(value)
+            if key == "body":
+                if len(value) > 25:
+                    value, city = remove_city(value)
+                    if city:
+                        prep_content_data["city"] = city
 
         if key in [
-            "author",
             "body",
             "title",
             "releaseDate",
